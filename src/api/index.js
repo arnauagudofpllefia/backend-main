d7dc986f65da5219cbd0c3974a0dc578b9a3050d
arnauagudofpllefia <arnagudo05@gmail.com>
2026-03-10 19:57:09 +0000

    .

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { connectDB } from '../../config/database.js';
+import { connectDB } from '../config/database.js';
 import alumnosRoutes from '../../routes/alumnos.routes.js';
 
 const app = express();
